acd5bf1559f862bbe08c082b6948dba0d95cbccd
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-19 23:57:14 +0530

    Time: 38 ms (99.14%), Space: 18 MB (49.5%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,29 +1,6 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         x = set(nums)
-        y = list(x)
-        def merge_sort(y):
-            if len(y) <= 1:
-                return y
-            mid = len(y) // 2
-            left = y[:mid]
-            right = y[mid:]
-            left = merge_sort(left)
-            right = merge_sort(right)
-            return merge(left, right)
-        def merge(left, right):
-            result = [] 
-            i = j = 0 
-            while i < len(left) and j < len(right):
-                if left[i] < right[j]:
-                    result.append(left[i])
-                    i += 1
-                else:
-                    result.append(right[j])
-                    j += 1
-            result.extend(left[i:])
-            result.extend(right[j:])
-            return result
-        y = merge_sort(y)
+        y = sorted(list(x))
         return y[-3] if len(y) >= 3 else y[-1]
         
