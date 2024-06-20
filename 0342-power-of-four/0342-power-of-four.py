44b8a338b0eb352cbe9dad6100ef0f3b3fd4b2e8
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-21 01:21:14 +0530

    Time: 37 ms (49.75%), Space: 16.3 MB (97.29%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         l = [4**x for x in range(16)]
-        if n not in l:
-            return False
-        return True
+        return n in l
         
