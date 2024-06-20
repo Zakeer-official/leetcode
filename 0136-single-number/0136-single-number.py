9e1dabb160557614b5ebb468f228d3175bcf4b29
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-21 00:08:29 +0530

    Time: 3784 ms (6.27%), Space: 18.9 MB (86.22%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        y = list(set(nums))
-        x = 0
-        for i in y:
+        for i in nums:
             if nums.count(i) == 1:
                 return i 
         
