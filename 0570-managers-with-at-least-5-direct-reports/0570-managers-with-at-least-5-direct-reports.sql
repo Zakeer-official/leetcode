50992322a33a2467c24f9e3ea43b53418e10dd64
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-22 00:01:02 +0530

    Time: 962 ms (37.71%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,1 +1,1 @@
-select a.name from Employee a,Employee b where a.id in b.managerId group by a.id,a.name having count(b.managerId) >= 5
+select a.name from Employee a,Employee b where a.id = b.managerId group by a.id,a.name having count(b.managerId) >= 5
