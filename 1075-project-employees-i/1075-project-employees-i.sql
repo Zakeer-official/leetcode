a98013662a26069f947010ca5884adcdfa1b8940
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-25 23:30:32 +0530

    Time: 499 ms (83.15%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,1 +1,2 @@
-select p.project_id , round(avg(e.experience_years),2) average_years from Project p left join Employee e on p.employee_id = e.employee_id group by p.project_id
+select p.project_id , round(avg(e.experience_years),2) average_years 
+from Project p left join Employee e on p.employee_id = e.employee_id group by p.project_id
