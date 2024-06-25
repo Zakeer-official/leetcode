99f0b435d8dd3ea519aec764b1c2fe67261133cc
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-25 23:58:38 +0530

    Time: 946 ms (83.68%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,1 @@
+select r.contest_id , round((count(r.user_id)/ (select count(*) from Users))*100,2) percentage from Users u right join Register r on u.user_id = r.user_id group by r.contest_id order by percentage desc,r.contest_id
