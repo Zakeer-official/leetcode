4346dbc81a1c986578a7c3eaf39150bc4efc90f9
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-27 22:54:10 +0530

    Time: 610 ms (77.72%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,1 @@
+select concat(substr(trans_date,1,7)) as 'month',country,count(state) trans_count,sum(case when state = 'approved' then 1 else 0 end) approved_count,sum(amount) trans_total_amount ,sum(case when state = 'approved' then amount else 0 end) approved_total_amount from Transactions group by month,country
