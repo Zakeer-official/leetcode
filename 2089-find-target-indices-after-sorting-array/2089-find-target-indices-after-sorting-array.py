be46e8376d7f8fb7dccac9d164cc89b7f4920c53
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-06-30 00:25:54 +0530

    Time: 52 ms (28.35%), Space: 16.7 MB (9.48%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        def merge(l, r):
+            merged = []
+            i = j = 0
+            while i < len(l) and j < len(r):
+                if l[i] < r[j]:
+                    merged.append(l[i])
+                    i += 1 
+                else:
+                    merged.append(r[j])
+                    j += 1
+            merged.extend(l[i:])
+            merged.extend(r[j:])
+            return merged
+                
+        def ms(arr):
+            if len(arr) <= 1:
+                return arr
+            
+            mid = len(arr) // 2
+            left = ms(arr[:mid])
+            right = ms(arr[mid:])
+            return merge(left, right)
+
+        y = ms(nums)
+        result = []
+        for i in range(len(y)):
+            if target == y[i]:
+                result.append(i)
+        return result
+        
