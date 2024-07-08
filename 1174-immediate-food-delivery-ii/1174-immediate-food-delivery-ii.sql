0fe5d45acf7763a1a18cda8ad92aed37b56df792
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-07-08 21:31:46 +0530

    Time: 1272 ms (22.15%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,1 @@
+select round((sum(case when order_date = customer_pref_delivery_date and order_date = any (select min(order_date) from Delivery b where a.customer_id = b.customer_id) then 1 else 0 end) / ( select count(distinct customer_id) from Delivery))*100,2) immediate_percentage from Delivery a;
