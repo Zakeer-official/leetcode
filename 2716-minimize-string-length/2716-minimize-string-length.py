44c06df2aa2b066c423621e22ff4ef134941071a
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-07-09 00:35:45 +0530

    Time: 39 ms (92.35%), Space: 16.5 MB (75.59%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.py
+++ 2716-minimize-string-length/2716-minimize-string-length.py
@@ -1,8 +1,4 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
-        x = []
-        for i in s:
-            if i not in x:
-                x.append(i)
-        return len(x)
+        return len(set(s))
         
