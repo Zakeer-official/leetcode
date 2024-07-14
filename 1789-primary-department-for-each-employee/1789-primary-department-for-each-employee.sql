4fbba423f8645de9f87c0950db56e19d41483421
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-07-14 21:45:18 +0530

    Time: 528 ms (61.64%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,11 +1,2 @@
-
-SELECT employee_id, department_id
-FROM Employee
-GROUP BY employee_id
-HAVING COUNT(employee_id) < 2
-
-UNION
-
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y';
+select employee_id , department_id from Employee where primary_flag = 'Y' or employee_id in
+(select employee_id from Employee group by employee_id having count(employee_id) = 1)
